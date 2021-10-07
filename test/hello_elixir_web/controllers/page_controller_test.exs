b7ef7b8736f2064fd9dad31f96534aedbbcc1126
kamidev <jonas.beckman@gmail.com>
2021-10-07 05:57:30 +0200

    Ignore irrelevant test

--- test/hello_elixir_web/controllers/page_controller_test.exs
+++ test/hello_elixir_web/controllers/page_controller_test.exs
@@ -1,6 +1,7 @@
 defmodule HelloElixirWeb.PageControllerTest do
   use HelloElixirWeb.ConnCase
 
+  @tag :skip
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
