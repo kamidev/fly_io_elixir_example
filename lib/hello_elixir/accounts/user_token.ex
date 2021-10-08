75a63f56ca350ca9f684f76abb47e2322a154256
kamidev <jonas.beckman@gmail.com>
2021-10-08 06:41:21 +0200

    Fix badly formatted file

--- lib/hello_elixir/accounts/user_token.ex
+++ lib/hello_elixir/accounts/user_token.ex
@@ -173,6 +173,7 @@ defmodule HelloElixir.Accounts.UserToken do
   end
 
   def user_and_contexts_query(user, [_ | _] = contexts) do
-    from t in HelloElixir.Accounts.UserToken, where: t.user_id == ^user.id and t.context in ^contexts
+    from t in HelloElixir.Accounts.UserToken,
+      where: t.user_id == ^user.id and t.context in ^contexts
   end
 end
